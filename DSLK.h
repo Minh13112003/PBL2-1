5cb244ec907f45ed66822f7099be7bbf71cd5a61
Minh13112003 <118954186+Minh13112003@users.noreply.github.com>
2022-11-29 16:36:36 +0700

    Add files via upload

--- DSLK.h
+++ DSLK.h
@@ -2,18 +2,16 @@
 #ifndef DSLK_H
 #define DSLK_H
 
-template <typename T>
+template < class T>
 struct Node
 {
 	T data;
 	Node* Next;
 	Node* Pre;
 	Node(T data)
-	{
-		this->data = data;
-		this->Next = this->Pre = nullptr;
-	}
 };
+
+
 template < class T>
 class List {
 
