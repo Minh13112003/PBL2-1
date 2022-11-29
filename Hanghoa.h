7312361fdd672fee60afc2db423c60079df375db
Minh13112003 <118954186+Minh13112003@users.noreply.github.com>
2022-11-29 22:33:46 +0700

    Add files via upload

---
+++ Hanghoa.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+#ifndef Hanghoa_H
+#define Hanghoa_H
+
+using namespace std;
+
+class HangHoa {
+	string tensp;
+	int soluong;
+	float giaban;
+	static int a;
+public:
+	HangHoa();
+	HangHoa(string, int, float);
+	void show();
+	friend istream& operator >> (istream& in, HangHoa& a);
+};
+
+#endif // !Hanghoa_H
+
+
+
---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include "DSLK.cpp"
+#include "Hanghoa.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	HangHoa x;
+	cin >> x;
+	List <HangHoa> a;
+	a.InsertLast((HangHoa) x);
+	a.Show();
+	
+}
