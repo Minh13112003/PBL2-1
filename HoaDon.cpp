acf4a28bd78735bd2bee833ea789ef8f2d7e9d4c
Minh13112003 <118954186+Minh13112003@users.noreply.github.com>
2022-12-14 01:46:03 +0700

    Add files via upload

--- HoaDon.cpp
+++ HoaDon.cpp
@@ -1,4 +1,4 @@
-
+#include <iomanip>
 #include <iostream>
 #include "HoaDon.h"
 #include <string>
@@ -8,10 +8,16 @@ using namespace std;
 int HoaDon::count = 1;
 
 
-void HoaDon::Input(List<HangHoa>& a,List <HangHoa> &b) {
+HoaDon::HoaDon() {
+	this->tongtien = 0;
+}
+
+void HoaDon::Input(List<HangHoa>& a, List <HangHoa>& b) {
 	cin.ignore();
 	cout << "nhap id khach hang: ";
 	cin >> this->id;
+	cout << "nhap so tien khach gui: ";
+	cin >> this->tiengui;
 	bool e = 1;
 	while (e == 1) {
 		cout << "nhap ten san pham muon mua: ";
@@ -22,6 +28,7 @@ void HoaDon::Input(List<HangHoa>& a,List <HangHoa> &b) {
 			cin.ignore();
 			ChangeSoluong(a, this->tensp, this->soluong);
 			this->giaban = this->soluong * Getgiaban(a, tensp);
+			tongtien += this->giaban;
 			HangHoa temp(this->tensp, this->soluong, this->giaban);
 			b.InsertLast(temp);
 			cout << "ban co muon tiep tuc mua khong: 1(co),0(khong): ";
@@ -38,6 +45,9 @@ void HoaDon::show( List <HangHoa> b) {
 		c->data.show();
 		c = c->Next;
 	}
-
+	cout << "-------------------------------------------------------------------------";
+	cout << "Tong tien: " << right << setw(45) << this->tongtien<< endl;
+	cout << "Tien khach gui: " << right << setw(45) << this->tiengui << endl;
+	cout << "So tien tra lai: " << right << setw(45) << tiengui - tongtien;
 
 }
